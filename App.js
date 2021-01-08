3f248f0b76287fe4a35825ca10dcd04e35a883dd
Beatriz Odorcik <70718398+beatrizodorcik@users.noreply.github.com>
2021-01-07 22:30:07 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import './App.css';
+import GithubImage from './github-logo.png';
+
+function App() {
+  const [search, setSearch] = useState('');
+  const [userData, setUserData] = useState();
+
+  const handSubmit = (event) => {
+    event.preventDefault();
+    fetch(`https://api.github.com/users/${search}`)
+      .then(response => response.json())
+      .then(userReponse => setUserData(userReponse));
+  }
+
+  console.log(userData);
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  }
+
+  return (
+    <div className="container text-center">
+      <h1 className="py-5 text-uppercase">GITHUB PROFILE</h1>
+      <form onSubmit={handSubmit}>
+        <div className="form-group">
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              required
+              value={search}
+              onChange={handleChange}
+            />
+            <span className="input-group-btn">
+              <button type="submit" className="btn btn-success">
+                Search
+              </button>
+            </span>
+          </div>
+        </div>
+      </form>
+      <div className="py-5">
+        {!userData && (
+          <img
+            src={GithubImage}
+            className="responsive rounded-circle"
+            alt=""
+            height="200px"
+          />
+        )}
+        {userData && (
+          <div>
+            <img
+              src={userData.avatar_url}
+              className="responsive rounded-circle"
+              alt=""
+              height="200px"
+            />
+            <h1 className="pt-6">
+              <a href="https://github.com/beatrizodorcik" target="_new">
+                {userData.name}
+             </a>
+            </h1>
+            <h3>{userData.location}</h3>
+            <p>
+              <a href={userData.blog} target="_new" className="text-info">
+                {userData.blog}
+                </a>
+            </p>
+          </div >
+        )}
+      </div >
+    </div >
+  );
+}
+
+export default App;
